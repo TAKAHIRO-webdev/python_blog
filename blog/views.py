155759080fe256c61e7faf7b3c66669bfd70b75a
TAKAHIRO-webdev <tk30.webdeveloper@gmail.com>
2019-10-05 03:14:34 +0900

    create django blog

--- blog/views.py
+++ blog/views.py
@@ -1,45 +1,47 @@
 from django.shortcuts import render
 from blog.models import Post
-from . import CommentForm
+from blog.forms import CommentForm
+from blog.models import Comment
 
 def blog_index(request):
-  posts = Post.objects.all().order_by('-created_on')
-  context = {
-    "posts": posts,
-  }
-return render(request, "blog_index.html", context)
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
 
 def blog_category(request, category):
-  posts = Post.objects.filter(
-    categories__name__contains=category
-  ).order_ by(
-    '-created_on'
-  )
-  context = {
-    "category": category,
-    "posts": posts
-  }
-  return render(request, "blog_category.html", context)
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
 
-  def blog_detail(request, pk)
-  post = Post.objects.get(pk=pk)
 
-form = CommentForm()
-if request.method ==  'POST':
-  form = CommentForm(request.POST)
-    if form.is_valid():
-      comment = Comment(
-        author=form.cleaned_data["author"],
-        body=form.cleaned_data["body"],
-        post=post
-      )
-      comment.save()
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
 
-  comments = comment.objects.filter(post=post)
-  context = {
-    "post": post,
-    "comments":comments, 
-    "form": form,
-  }
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
 
-  return render(request, "blog_detail.html", context)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+    }
+    return render(request, "blog_detail.html", context)
